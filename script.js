5a978e9254eb85af6566c19f2e8cc981bb99ca80
Shivam Kumar <shivamkumar271855@gmail.com>
2023-01-16 22:56:33 +0530

    Marketing Template website

---
+++ script.js
@@ -0,0 +1,20 @@
+let slideIndex = 1;
+showSlide(slideIndex);
+
+function plusSlide(n){
+    showSlide(slideIndex += n);
+}
+function showSlide(n){
+    let i;
+    let slide = document.getElementsByClassName('mySlide');
+    if(n>slide.length){
+        slideIndex = 1;
+    }
+    if(n<1){
+        slideIndex = slide.length;
+    }
+    for(i = 0; i<slide.length; i++){
+        slide[i].style.display = 'none';
+    }
+    slide[slideIndex-1].style.display = 'block';
+}
